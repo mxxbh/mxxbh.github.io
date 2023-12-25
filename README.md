6369fa645791e76c0a5cf88fe959c79161ab1b2d
何沁 <heqin@sheencity.com>
2023-12-25 21:43:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 网址
+
+github 免费静态网站
+
+--> [mxxbh.github.io](http://mxxbh.github.io)
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'homepage.html'),
+      },
+    },
+    outDir: 'pages',
+  },
+});
+
