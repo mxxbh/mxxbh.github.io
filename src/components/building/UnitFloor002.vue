fc74886bcfade52ffada0817e5d9c6f0e8e0298b
HeQin <heqin3.14@outlook.com>
2026-04-13 14:50:51 +0800

    faet: 添加github主页链接

--- src/components/building/UnitFloor002.vue
+++ src/components/building/UnitFloor002.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import githubIcon from "../../../public/github.svg";
+
+const handleClick = () => {
+  window.open("https://github.com/mxxbh/mxxbh.github.io", "_self");
+};
+
 </script>
 
 <template>
-  <div class="building-floor floor002"></div>
+  <div class="building-floor floor002">
+    <img :src="githubIcon" @click.stop="handleClick" alt="github" />
+  </div>
 </template>
 
 <style scoped lang="scss">
 .floor002 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 15%;
+    aspect-ratio: 1;
+    object-fit: contain;
+  }
 }
 </style>
